1dabe25e9249c0b0787172a9388b24026e0729f1
A48264 - João Pereira <a48264@alunos.isel.pt>
2024-02-27 18:45:53 +0000

    Phase1 sql updates

--- sql/phase1/createTables.sql
+++ sql/phase1/createTables.sql
@@ -1,25 +1,25 @@
 CREATE TABLE Player(
-    id SERIAL PRIMARY KEY START WITH 1000,
+    pid INT GENERATED ALWAYS AS IDENTITY (START WITH 1000) PRIMARY KEY,
     name VARCHAR(50),
     email VARCHAR(50) UNIQUE
 );
 
 CREATE TABLE Game(
-    id SERIAL PRIMARY KEY START WITH 100,
+    gid INT GENERATED ALWAYS AS IDENTITY (START WITH 100) PRIMARY KEY,
     name VARCHAR(50) UNIQUE,
     developer VARCHAR(50),
     genres VARCHAR(20)[]
 );
 
 CREATE TABLE Session(
-    id SERIAL PRIMARY KEY START WITH 10000,
+    sid INT GENERATED ALWAYS AS IDENTITY (START WITH 10000) PRIMARY KEY,
     capacity INT,
     session_date DATE,
-    game_id INT REFERENCES Game(id)
+    game_id INT REFERENCES Game(gid)
 );
 
 CREATE TABLE SessionPlayer(
-    session_id INT REFERENCES Session(id),
-    player_id INT REFERENCES Player(id),
+    session_id INT REFERENCES Session(sid),
+    player_id INT REFERENCES Player(pid),
     PRIMARY KEY (session_id, player_id)
 );
--- sql/phase1/insertData.sql
+++ sql/phase1/insertData.sql
@@ -1,29 +1,31 @@
-INSERT INTO Player(name, email)
-VALUES 
-('Pedro', 'pedro@hotmail.com'),
-('Vasco', 'vasco@gmail.com'),
-('Jonny', 'jonny@live.com.pt');
-
-INSERT INTO Game(id, name, genres)
-VALUES 
-(1,'Lord of the Rings Online', ARRAY['RPG', 'Adventure']),
-(2,'Apex Legends', ARRAY['Shooter', 'Action']),
-(3,'Civilization VI', ARRAY['Turn-Based', 'Strategy']);
-
-INSERT INTO Session (id, number_of_players, session_date, game_id)
-VALUES 
-(1, 3, '2024-02-25', 1),
-(2, 3, '2024-02-26', 2),
-(3, 2, '2024-02-27', 3);
+-- Insert into Player table
+INSERT INTO Player(name, email) VALUES 
+	('Pedro', 'pedro@hotmail.com'),
+	('Vasco', 'vasco@gmail.com'),
+	('Jonny', 'jonny@live.com.pt');
 
 
-INSERT INTO Player_Session (session_id, player_id) 
-VALUES 
-(1, 1),
-(1, 2),
-(1, 3),
-(2, 1),
-(2, 2),
-(2, 3),
-(3, 1),
-(3, 3);
+-- Insert into Game table
+INSERT INTO Game(name, developer, genres) VALUES 
+	('Lord of the Rings Online', NULL, ARRAY['RPG', 'Adventure']),
+	('Apex Legends', NULL, ARRAY['Shooter', 'Action']),
+	('Civilization VI', NULL, ARRAY['Turn-Based', 'Strategy']);
+
+
+-- Insert into Session table
+INSERT INTO Session(capacity, session_date, game_id) VALUES 
+	(3, '2024-02-25', 100),
+	(3, '2024-02-26', 101),
+	(2, '2024-02-27', 102);
+
+
+-- Insert into SessionPlayer table
+INSERT INTO SessionPlayer(session_id, player_id) VALUES 
+	(10000, 1000),
+	(10000, 1001),
+	(10000, 1002),
+	(10001, 1000),
+	(10001, 1001),
+	(10001, 1002),
+	(10002, 1000),
+	(10002, 1002);
