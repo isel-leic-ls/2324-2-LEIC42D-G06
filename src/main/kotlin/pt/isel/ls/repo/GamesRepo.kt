c044a7f72ffdb030f3cef713dcdb11b666d24863
A48264 - João Pereira <a48264@alunos.isel.pt>
2024-02-29 02:14:33 +0000

    Update GamesRepo.kt
    Interface created and commented.

--- src/main/kotlin/pt/isel/ls/repo/GamesRepo.kt
+++ src/main/kotlin/pt/isel/ls/repo/GamesRepo.kt
@@ -2,8 +2,17 @@ package pt.isel.ls.repo
 
 import pt.isel.ls.domain.Game
 
+
 interface GamesRepo {
-    fun createGame(name : String, developer : String, genres : Array<String>) : Int
-    fun getGame(gid : Int) : Game
-    fun getListOfGames(genres : Array<String>, developer : String) : List<Game>
+    /** returns the id of the inserted game */
+    fun insert(name: String, developer: String, genres: List<String>): Int
+
+    /** get details of a game by its id */
+    fun getGameById(gid: Int): Game
+
+    /** get details of a game by its name */
+    fun getGameByName(name: String): Game
+
+    /** get a list made by the list of games of given genre(s) plus the list of games of the given developer */
+    fun getListOfGames(genres: List<String>, developer: String): List<Game>
 }
