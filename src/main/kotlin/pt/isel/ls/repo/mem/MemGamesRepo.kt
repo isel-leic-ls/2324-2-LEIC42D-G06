9da25b7f221d046c58fb2e670796363158c2b376
A48264 - João Pereira <a48264@alunos.isel.pt>
2024-03-04 17:11:25 +0000

    Removed redundancy in handling concurrency

--- src/main/kotlin/pt/isel/ls/repo/mem/MemGamesRepo.kt
+++ src/main/kotlin/pt/isel/ls/repo/mem/MemGamesRepo.kt
@@ -5,8 +5,6 @@ import pt.isel.ls.repo.Exceptions
 import pt.isel.ls.repo.interfaces.GamesRepo
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicInteger
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
 
 
 const val INITIAL_GAME_ID = 100
@@ -14,25 +12,19 @@ const val INITIAL_GAME_ID = 100
 class MemGamesRepo : GamesRepo {
     private val games = ConcurrentLinkedQueue<Game>()
     private val currentId = AtomicInteger(INITIAL_GAME_ID)
-    private val lock = ReentrantLock()
 
     override fun insert(name: String, developer: String, genres: List<String>): Int {
-        lock.withLock {
-            val gameId = currentId.getAndIncrement()
-            games.add(Game(gameId, name, developer, genres))
-            return gameId
-        }
+        val gameId = currentId.getAndIncrement()
+        games.add(Game(gameId, name, developer, genres))
+        return gameId
     }
 
-    override fun getGameById(gid: Int): Game = lock.withLock {
+    override fun getGameById(gid: Int): Game =
         games.find { it.id == gid } ?: throw Exceptions.GameNotFound("Game not found with id $gid")
-    }
 
-    override fun getGameByName(name: String): Game = lock.withLock {
+    override fun getGameByName(name: String): Game =
         games.find { it.name == name } ?: throw Exceptions.GameNotFound("Game not found with name $name")
-    }
 
-    override fun getListOfGames(genres: List<String>, developer: String): List<Game> = lock.withLock {
+    override fun getListOfGames(genres: List<String>, developer: String): List<Game> =
         games.filter { it.dev == developer || it.genres.any { g -> genres.contains(g) } }
-    }
 }
