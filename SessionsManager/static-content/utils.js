7f6fed6025d1d9ff283ba90aac66bde88c934c14
A48264 - João Pereira <a48264@alunos.isel.pt>
2024-04-08 21:11:49 +0100

    Update utils.js

--- SessionsManager/static-content/utils.js
+++ SessionsManager/static-content/utils.js
@@ -1,5 +1,10 @@
 export const CONSTS = {
-    BASE_API_URL: "http://localhost:9000/api"
+    BASE_API_URL: "http://localhost:9000/api",
+    FIRST_GAME_ID: 100,
+    FIRST_PLAYER_ID: 1000,
+    FIRST_SESSION_ID: 10000,
+    SKIP_DEFAULT: 0,
+    LIMIT_DEFAULT: 5
 }
 
 export default CONSTS;
