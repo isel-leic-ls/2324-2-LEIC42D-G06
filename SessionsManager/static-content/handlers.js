1e22037d0965acd2c4e821f7ff734fb18de4f62b
Vasco Branco <80894056+Vaasco@users.noreply.github.com>
2024-04-09 23:40:59 +0100

    -Implemented safe call scope function in handler.js

--- SessionsManager/static-content/handlers.js
+++ SessionsManager/static-content/handlers.js
@@ -1,26 +1,32 @@
-import { homePage } from "./pages/homePage.js"
-import { gamesSearchPage, gamesListPage, gameDetailsPage } from "./pages/gamesPages.js"
-import { sessionsSearchPage, sessionsListPage, sessionDetailsPage } from "./pages/sessionsPages.js"
-import { playerDetailsPage } from "./pages/playerPages.js"
-import { pagingButtons } from "./components/pagingButtons.js"
-import { basicError } from "./components/basicError.js"
-import { filterQueryParameters, filterUriId } from "./uriparsers.js"
-import { sessionsRetrieval, sessionDetailsRetrieval } from "./services/sessionServices.js"
-import { gamesRetrieval, gameDetailsRetrieval } from "./services/gamesServices.js"
-import { playerDetailsRetrieval, playerIdRetrieval } from "./services/playerServices.js"
+import {homePage} from "./pages/homePage.js"
+import {gamesSearchPage, gamesListPage, gameDetailsPage} from "./pages/gamesPages.js"
+import {sessionsSearchPage, sessionsListPage, sessionDetailsPage} from "./pages/sessionsPages.js"
+import {playerDetailsPage} from "./pages/playerPages.js"
+import {pagingButtons} from "./components/pagingButtons.js"
+import {basicError} from "./components/basicError.js"
+import {filterQueryParameters, filterUriId} from "./uriparsers.js"
+import {sessionsRetrieval, sessionDetailsRetrieval} from "./services/sessionServices.js"
+import {gamesRetrieval, gameDetailsRetrieval} from "./services/gamesServices.js"
+import {playerDetailsRetrieval, playerIdRetrieval} from "./services/playerServices.js"
+
+
+async function safeCall(mainContent, block){
+    try{
+        await block()
+    }catch(error){
+        const errorContent = basicError(error.message)
+        mainContent.replaceChildren(errorContent)
+    }
+}
 
 
 /** Home */
 async function getHome(mainContent) {
-    try {
+    safeCall(mainContent, async ()=> {
         const player = await playerIdRetrieval()
         const pageContent = homePage(player);
         mainContent.replaceChildren(pageContent);
-    } catch (error) {
-        console.error(error);
-        const errorContent = basicError(error.message);
-        mainContent.replaceChildren(errorContent);
-    }
+    })
 }
 
 /** Games */
@@ -30,32 +36,22 @@ function getGamesSearch(mainContent) {
 }
 
 async function getGamesList(mainContent, path) {
-    try {
-        const { skip, limit, genres, developer } = filterQueryParameters(path);
-        const { games, total } = await gamesRetrieval(skip, limit, genres, developer);
+    safeCall(mainContent, async ()=> {
+        const {skip, limit, genres, developer} = filterQueryParameters(path);
+        const {games, total} = await gamesRetrieval(skip, limit, genres, developer);
         const buttons = pagingButtons(parseInt(skip), parseInt(limit), total, path)
         const pageContent = gamesListPage(games, buttons);
         mainContent.replaceChildren(pageContent);
-    }
-    catch (error) {
-        console.error(error);
-        const errorContent = basicError(error.message);
-        mainContent.replaceChildren(errorContent);
-    }
+    })
 }
 
 async function getGameDetails(mainContent, path) {
-    try {
+    safeCall(mainContent,async ()=> {
         const gid = filterUriId(path);
         const game = await gameDetailsRetrieval(gid);
         const pageContent = gameDetailsPage(game);
         mainContent.replaceChildren(pageContent);
-    }
-    catch (error) {
-        console.error(error);
-        const errorContent = basicError(error.message);
-        mainContent.replaceChildren(errorContent);
-    }
+    })
 
 }
 
@@ -66,44 +62,32 @@ function getSessionsSearch(mainContent) {
 }
 
 async function getSessionsList(mainContent, path) {
-    try {
-        const { gid, date, state, pid, skip, limit } = filterQueryParameters(path);
-        const { sessions, total } = await sessionsRetrieval(gid, date, state, pid, skip, limit);
+    safeCall(mainContent,async ()=> {
+        const {gid, date, state, pid, skip, limit} = filterQueryParameters(path);
+        const {sessions, total} = await sessionsRetrieval(gid, date, state, pid, skip, limit);
         const buttons = pagingButtons(parseInt(skip), parseInt(limit), total, path)
         const pageContent = sessionsListPage(sessions, buttons);
         mainContent.replaceChildren(pageContent);
-    } catch (error) {
-        console.error(error);
-        const errorContent = basicError(error.message);
-        mainContent.replaceChildren(errorContent);
-    }
+    })
 }
 
 async function getSessionDetails(mainContent, path) {
-    try {
+    safeCall(mainContent,async ()=>{
         const sid = filterUriId(path);
         const result = await sessionDetailsRetrieval(sid);
         const pageContent = sessionDetailsPage(result.session);
         mainContent.replaceChildren(pageContent);
-    } catch (error) {
-        console.error(error);
-        const errorContent = basicError(error.message);
-        mainContent.replaceChildren(errorContent);
-    }
+    })
 }
 
 /** Players */
 async function getPlayer(mainContent, path) {
-    try {
+    safeCall(mainContent,async ()=> {
         const pid = filterUriId(path);
         const player = await playerDetailsRetrieval(pid);
         const pageContent = playerDetailsPage(player);
         mainContent.replaceChildren(pageContent);
-    } catch (error) {
-        console.error(error);
-        const errorContent = basicError(error.message);
-        mainContent.replaceChildren(errorContent);
-    }
+    })
 }
 
 export const handlers = {
